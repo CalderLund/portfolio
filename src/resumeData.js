b2870f92970dee85ed993da308b7bb27af602672
CalderLund <cjlund@uwaterloo.ca>
2021-04-22 13:57:20 -0400

    Build initial website design

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://calderlund.github.io/portfolio/",
+    "name": "Calder Lund",
+    "role": "4th year Computer Scientist at the University of Waterloo",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": " I enjoy working on projects that challenge me. I am rarely satisfied with what I have accomplished knowing there is so much more that can always be done.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/calder-lund-874381156/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/CalderLund",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"youtube",
+          "url":"https://www.youtube.com/user/TheBurntLemons",
+          "className":"fa fa-youtube"
+        }
+      ],
+    "aboutme":"I am a former competitive hockey player, musician, film creator and computer scientist. I currently specialize in Infrastructure Engineering, but I have worked on a variety of projects that include machine learning, website design, software security, computer networks and much more. Outside of school, I spend most of my time playing basketball, writing music or improving my slightly above mediocre chess skills.",
+    "address":"Toronto, Canada",
+    "website":"cjlund@uwaterloo.ca",
+    "education":[
+      {
+        "UniversityName":"University of Waterloo",
+        "specialization":"Bachelors of Computer Science",
+        "MonthOfPassing":"Expected April",
+        "YearOfPassing":"2022",
+        "Achievements":[
+          "GPA of 3.6/4.0",
+          "Minor in Combinatorics and Optimization",
+          "Business Option"
+        ]
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Wish (ContextLogic Inc.)",
+        "specialization":"Infrastructure Security Engineer",
+        "MonthOfLeaving":"May 2020 - Aug 2021",
+        "YearOfLeaving":"",
+        "Achievements":[
+          "Secured AWS subnet communication across all accounts by building automated NACL tooling using HashiCorp Terraform and Python",
+          "Wrote and deployed HashiCorp Vault client management tools using Golang and Python to store secrets securely",
+          "Created authentication/authorization tools for MongoDB clients to allow for improved metrics and control over user/service access rights",
+          "Automated Kubernetes deployments using JSONNET",
+          "Built secure repository to authenticate Dex, Vault and AWS clients"
+        ]
+      },
+      {
+        "CompanyName":"BlackBerry Limited",
+        "specialization":"Software Security Developer",
+        "MonthOfLeaving":"Sep 2019 - Dec 2019",
+        "YearOfLeaving":"",
+        "Achievements":[
+          "Designed and implemented a patent-pending machine learning algorithm for action recognition in the automotive field",
+          "Developed production-level cybersecurity software for Android and iOS devices using C++",
+          "Built testing framework for team to allow for easy unit testing allowing for long-term sustainability of code",
+          "Found and fixed critical bugs before major release date ensuring product met product requirements",
+          "Worked in fast-paced Agile environment on scrum team"
+        ]
+      },
+      {
+        "CompanyName":"DaganTech (formerly AgriLogicAI)",
+        "specialization":"Machine Learning Product Developer",
+        "MonthOfLeaving":"Jan 2019 - Apr 2019",
+        "YearOfLeaving":"",
+        "Achievements":[
+          "Solved problems in agriculture using computer vision, time-series analysis and other machine learning techniques",
+          "Designed a FCN-LSTM capable of predicting a farm’s crop growth allowing for easy anomaly detection",
+          "Performed data analysis using Python, Jupyter Notebooks and R"
+        ]
+      },
+      {
+        "CompanyName":"CIBC Capital Markets",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"May 2018 - Aug 2018",
+        "YearOfLeaving":"",
+        "Achievements":[
+          "Created hedging solutions in finance space using supervised learning and technical indicators",
+          "Prototyped and designed end to end application that provides statistical analysis tools for traders",
+          "Worked on trade-floor side-by-side with liability traders"
+        ]
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"UWPath",
+        "description":"Web application designed to help UWaterloo students plan their courses properly, following their majors, minors and options.",
+        "techstack":"Python, Vue.js, Django, Docker, Kubernetes, Bash, AWS, CircleCi, PostgreSQL",
+        "imgurl":"images/greyImage.png"
+      }
+    ]
+  }
+  
+  export default resumeData
